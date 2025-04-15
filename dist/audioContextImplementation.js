d521f7d28e56364d65a92960b66a96bb371b85a7
prabhat24 <prabhat24katiyar@gmail.com>
2025-04-15 15:36:56 +0530

    commit

--- dist/audioContextImplementation.js
+++ dist/audioContextImplementation.js
@@ -6,6 +6,20 @@ let unlocked = false;
 let noiseSource;
 let dest;
 
+function logMessage(message, type="info") {
+  const logContainer = document.getElementById("log-container");
+  const logEntry = document.createElement("div");
+  logEntry.classList.add("log-entry");
+
+  if (type === "error") logEntry.classList.add("log-error");
+  else if (type === "info") logEntry.classList.add("log-info");
+  else logEntry.classList.add("log-debug");
+
+  logEntry.innerText = `[${new Date().toLocaleTimeString()}] ${message}`;
+  logContainer.appendChild(logEntry);
+  logContainer.scrollTop = logContainer.scrollHeight;
+}
+
 
 const whiteNoiseAudio = () => {
     if (!unlocked) {
@@ -32,7 +46,7 @@ const whiteNoiseAudio = () => {
       noiseSource.connect(gainNode);
       gainNode.connect(dest);
       noiseSource.start(0);
-      console.log("🌫️ Low-volume noise loop started");
+      logMessage("🌫️ Low-volume noise loop started");
       return dest
     }
   };
@@ -40,18 +54,16 @@ const whiteNoiseAudio = () => {
 
 async function playAudioFromFile(url, loop) {
   if (!audioContext || !dest) {
-    console.warn("Audio context not initialized yet.");
+    logMessage("Audio context not initialized yet.", "error");
     return;
   }
 
   try {
-    // Load the audio file
-    // const response = await fetch(url, );
-    // const arrayBuffer = await response.arrayBuffer();
-    // const audioBuffer = await audioContext.decodeAudioData(arrayBuffer);
-    const audioElement = new Audio(url);
+    
+    const audioElement = new Audio();
+    audioElement.src = url
     audioElement.crossOrigin = "anonymous";
-  
+    audioElement.load()
     const source = audioContext.createMediaElementSource(audioElement);
     // Create source and gain
     // const source = audioContext.createBufferSource();
@@ -67,13 +79,13 @@ async function playAudioFromFile(url, loop) {
     gainNode.connect(dest);
 
     source.start();
-    console.log("🎵 Audio file playing");
+    logMessage("Mixing 2 audio. Beep sound mixed");
 
     // Optional: return the source to stop it manually later
     return source;
   } 
   catch (err) {
-    console.error("Failed to load or play audio:", err);
+    logMessage("Failed to load or play audio:", "error");
   }
 }
 
